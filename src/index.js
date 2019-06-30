66ba3c1dc6ef3bc5c0c13ce384953ff48b8e20c5
Junior Dugue <jrdugue@gmail.com>
2019-06-30 13:47:34 -0400

    created a higher component to pass props data via the pokedex.js
    created a higher component to pass props data via the pokedex.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pokecard from "./components/Pokecard";
+// import Pokecard from "./components/Pokecard";
+import Pokedex from "./components/Pokedex";
+
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <Pokecard id={7} name="Squirtle" type="water" exp={63} />
-      <Pokecard id={4} name="Charmander" type="fire" exp={62} />
-      
+      {/* <Pokecard id={7} name="Squirtle" type="water" exp={63} /> */}
+      {/* <Pokecard id={4} name="Charmander" type="fire" exp={62} /> */}
+      <Pokedex />
     </div>
   );
 }
