57eb4eff81c5d5a304fa50a3df9cc6a83218cfa8
Junior Dugue <jrdugue@gmail.com>
2019-06-30 14:31:56 -0400

    styled the pokedex and pokecard components
    styled the pokedex and pokecard components

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -9,10 +9,10 @@ class Pokecard extends Component {
     let imgSrc = `${POKE_API}${this.props.id}.png`;
     return (
       <div className="Pokecard">
-        <h2>Name: {this.props.name}</h2>
+        <h2 className="Pokecard-title">Name: {this.props.name}</h2>
         <img src={imgSrc} alt={this.props.name} />
-        <p>Type: {this.props.type}</p>
-        <p>Experience: {this.props.exp}</p>
+        <p className="Pokecard-data">Type: {this.props.type}</p>
+        <p className="Pokecard-data">Experience: {this.props.exp}</p>
       </div>
     );
   }
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -19,9 +19,11 @@ class Pokedex extends Component {
     return (
       <div className="Pokedex">
         <h1>PokeDex!</h1>
+        <div className="Pokedex-cards">
         {this.props.pokemon.map(p => (
           <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-        ))}
+          ))}
+        </div>
       </div>
     );
   }
