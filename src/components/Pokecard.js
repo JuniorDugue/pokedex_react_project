c037d4cbe43987832c530c32702cc8d48117b745
Junior Dugue <jrdugue@gmail.com>
2019-06-30 13:31:11 -0400

    added alt tag to images and styling to the pokecard component
    added alt tag to images and styling to the pokecard component

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
+import "./pokecard.css";
 
-const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokecard extends Component {
-
   render() {
     let imgSrc = `${POKE_API}${this.props.id}.png`;
     return (
       <div className="Pokecard">
         <h2>Name: {this.props.name}</h2>
-        <img src={imgSrc}/>
+        <img src={imgSrc} alt={this.props.name} />
         <p>Type: {this.props.type}</p>
         <p>Experience: {this.props.exp}</p>
       </div>
