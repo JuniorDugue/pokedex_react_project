dc034a314d870a1e766931bd6237fa31c362f72b
Junior Dugue <jrdugue@gmail.com>
2019-07-01 23:34:52 -0400

    added styling
    added more styling

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -13,7 +13,9 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h2 className="Pokecard-title">Name: {this.props.name}</h2>
-        <img src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
         <p className="Pokecard-data">Type: {this.props.type}</p>
         <p className="Pokecard-data">Experience: {this.props.exp}</p>
       </div>
